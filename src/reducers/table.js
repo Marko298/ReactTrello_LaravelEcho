7b342206ddbd8bc1317d1f9244ab7fe21a787201
Mark Boychuk <boychuk.m.n@gmail.com>
2018-02-09 14:51:46 +0200

    added position update

--- src/reducers/table.js
+++ src/reducers/table.js
@@ -32,9 +32,20 @@ const table = (state = initialState, action) => {
         case types.CARD_MOVE:
             const e = state
                 .entities
+                .update('cards', cards => cards.merge(
+                    cards,
+                    cards
+                        .filter(card => card.get('column_id') === action.sColId && card.get('position') > action.sIndex)
+                        .map(card => card.update('position', position => position - 1)),
+                    cards
+                        .filter(card => card.get('column_id') === action.dColId && card.get('position') >= action.dIndex)
+                        .map(card => card.update('position', position => position + 1)),
+                    )
+                )
                 .deleteIn(['columns', action.sColId.toString(), 'cards', action.sIndex])
                 .updateIn(['columns', action.dColId.toString(), 'cards'], (cards) => cards.insert(action.dIndex, parseInt(action.cardId, 10)))
                 .updateIn(['cards', action.cardId.toString()], card => card.set('position', action.dIndex).set('column_id', action.dColId));
+
 
             return {
                 ...state,
@@ -44,9 +55,19 @@ const table = (state = initialState, action) => {
         case types.CARD_MOVE_FAILED:
             const e2 = state
                 .entities
-                .setIn(['cards', action.cardId, 'position'], action.sIndex)
+                .update('cards', cards => cards.merge(
+                    cards,
+                    cards
+                        .filter(card => card.get('column_id') === action.sColId && card.get('position') >= action.sIndex)
+                        .map(card => card.update('position', position => position + 1)),
+                    cards
+                        .filter(card => card.get('column_id') === action.dColId && card.get('position') > action.dIndex)
+                        .map(card => card.update('position', position => position - 1)),
+                    )
+                )
                 .deleteIn(['columns', action.dColId.toString(), 'cards', action.dIndex])
-                .updateIn(['columns', action.sColId.toString(), 'cards'], (cards) => cards.insert(action.sIndex, parseInt(action.cardId, 10)));
+                .updateIn(['columns', action.sColId.toString(), 'cards'], (cards) => cards.insert(action.sIndex, parseInt(action.cardId, 10)))
+                .updateIn(['cards', action.cardId.toString()], card => card.set('position', action.sIndex).set('column_id', action.sColId));
 
             return {
                 ...state,
